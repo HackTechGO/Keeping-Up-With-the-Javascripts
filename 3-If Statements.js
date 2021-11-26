dcbafa913a9d0890ba518e6f382f1874ff8855f8
AlizaminJ <jafarli_alizamin@yahoo.com>
2021-11-27 00:03:05 +0100

    update

--- 3-If Statements.js
+++ 3-If Statements.js
@@ -3,12 +3,12 @@
  *
  */
 
- // Strict equality operator
+// Strict equality operator
 
 const shouldSayHi = true;
 
 if (shouldSayHi === true) {
-  alert('Hi there')
+  alert('Hi there');
 }
 
 // If statement
@@ -19,17 +19,15 @@ if (shouldSayHi === true) {
 // evaluates to truthy or falsey
 // }
 
-
-
 const bankAccountBalance = 100;
 const costOfItem = 123;
 const hasCreditCard = true;
 const hasReachedCreditLimit = true;
 
 if (bankAccountBalance >= costOfItem) {
-  console.log("Congrats! You can pay for this with cash!")
-} else if (hasCreditCard && !hasReachedCreditLimit){
-  console.log("You can pay for this with credit")
+  console.log('Congrats! You can pay for this with cash!');
+} else if (hasCreditCard && !hasReachedCreditLimit) {
+  console.log('You can pay for this with credit');
 } else {
-  console.log("Sorry, you cannot purchase this item! :(")
+  console.log('Sorry, you cannot purchase this item! :(');
 }
